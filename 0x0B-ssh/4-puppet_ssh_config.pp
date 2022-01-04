dbfa754217516388e97b35e73d81fd3a6cc56392
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-01-04 12:07:51 -0800

    changing 4th task

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,4 +1,5 @@
-# sets up a client SSH configuration file so that we can connect to a server without using a password
+# sets up a client SSH configuration file so that we can connect to a
+# server without using a password
 include stdlib
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
